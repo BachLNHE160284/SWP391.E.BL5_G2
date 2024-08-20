fecf3e03cecfccf76886c2e1df6643c96fd35a6f
thanhmthe161167 <thanhmthe161167@fpt.edu.vn>
2024-08-20 12:59:59 +0700

    Staff reservation list

---
+++ src/java/dal/ReservationDAO.java
@@ -0,0 +1,54 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dal;
+
+import context.DBContext;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import model.ReservationDTO;
+
+/**
+ *
+ * @author Admin
+ */
+public class ReservationDAO extends DBContext {
+
+    public List<ReservationDTO> getAll(String from, String to) {
+        List<ReservationDTO> reservationDTOs = new ArrayList<>();
+        try {
+            String sql = "SELECT r.reservation_id ,  r.totalcost , r.reservation_date , u.email_address , r.status_reservation\n"
+                    + "FROM reservation r \n"
+                    + "left join [user] u on r.user_id = u.user_id\n"
+                    + "WHERE r.reservation_date >= ? and r.reservation_date <= ?";
+            ResultSet rs = null;
+            PreparedStatement ps = null;
+            Connection conn = null;
+
+            conn = new DBContext().getConnection(); // Open connection to SQL
+            ps = conn.prepareStatement(sql);
+            ps.setString(1, from);
+            ps.setString(2, to);
+            rs = ps.executeQuery();
+            while(rs.next()){
+               ReservationDTO dTO = new ReservationDTO();
+               dTO.setId(rs.getInt("reservation_id"));
+               dTO.setTotalCost(rs.getDouble("totalcost"));
+               dTO.setReservationDate(rs.getDate("reservation_date"));
+               dTO.setUserMail(rs.getString("email_address"));
+               dTO.setStatus(rs.getInt("status_reservation"));
+               reservationDTOs.add(dTO);
+            }
+            int b = 1;
+        } catch (Exception ex) {
+            Logger.getLogger(ReservationDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return reservationDTOs;
+    }
+}
---
+++ src/java/model/ReservationDTO.java
@@ -0,0 +1,62 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+import java.util.Date;
+
+/**
+ *
+ * @author Admin
+ */
+public class ReservationDTO {
+    private int id;
+    private double totalCost;
+    private Date reservationDate;
+    private String userMail;
+    private int status;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public double getTotalCost() {
+        return totalCost;
+    }
+
+    public void setTotalCost(double totalCost) {
+        this.totalCost = totalCost;
+    }
+
+    public Date getReservationDate() {
+        return reservationDate;
+    }
+
+    public void setReservationDate(Date reservationDate) {
+        this.reservationDate = reservationDate;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public String getUserMail() {
+        return userMail;
+    }
+
+    public void setUserMail(String userMail) {
+        this.userMail = userMail;
+    }
+    
+    
+    
+}
